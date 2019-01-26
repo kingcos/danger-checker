27c0bbda8cbb8350c280f332d849f13e0318a649
kingcos <2821836721v@gmail.com>
2019-01-26 17:30:40 +0800

    Update test spec

--- spec/text_checker_spec.rb
+++ spec/text_checker_spec.rb
@@ -14,33 +14,13 @@ module Danger
     describe "with Dangerfile" do
       before do
         @dangerfile = testing_dangerfile
-        @my_plugin = @dangerfile.text_checker
+        @text_checker = @dangerfile.text_checker
 
         # mock the PR data
         # you can then use this, eg. github.pr_author, later in the spec
-        json = File.read(File.dirname(__FILE__) + "/support/fixtures/github_pr.json") # example json: `curl https://api.github.com/repos/danger/danger-plugin-template/pulls/18 > github_pr.json`
-        allow(@my_plugin.github).to receive(:pr_json).and_return(json)
-      end
-
-      # Some examples for writing tests
-      # You should replace these with your own.
-
-      it "Warns on a monday" do
-        monday_date = Date.parse("2016-07-11")
-        allow(Date).to receive(:today).and_return monday_date
-
-        @my_plugin.warn_on_mondays
-
-        expect(@dangerfile.status_report[:warnings]).to eq(["Trying to merge code on a Monday"])
-      end
-
-      it "Does nothing on a tuesday" do
-        monday_date = Date.parse("2016-07-12")
-        allow(Date).to receive(:today).and_return monday_date
-
-        @my_plugin.warn_on_mondays
-
-        expect(@dangerfile.status_report[:warnings]).to eq([])
+        json = File.read(File.dirname(__FILE__) + "/support/fixtures/github_pr.json")
+        # example json: `curl https://api.github.com/repos/danger/danger-plugin-template/pulls/18 > github_pr.json`
+        allow(@text_checker.github).to receive(:pr_json).and_return(json)
       end
     end
   end
