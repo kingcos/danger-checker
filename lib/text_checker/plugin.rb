98d81be4300a25c0eccfc70801de1690d4ad5365
kingcos <2821836721v@gmail.com>
2019-01-26 17:31:49 +0800

    Add main funcs

--- lib/text_checker/plugin.rb
+++ lib/text_checker/plugin.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
 module Danger
   # Simply check your text as you like.
@@ -14,16 +14,67 @@ module Danger
   # @tags ci, danger-plugin, checker
   #
   class DangerTextChecker < Plugin
-    # An attribute that you can read/write from your Dangerfile
-    #
-    # @return   [Array<String>]
-    attr_accessor :my_attribute
+    attr_accessor :matched_files
+    attr_writer :rules_file_path
+    attr_writer :rules_pattern
 
-    # A method that you can call from your Dangerfile
-    # @return   [Array<String>]
+    # Setup the file regex patterns as you need
     #
-    def warn_on_mondays
-      warn "Trying to merge code on a Monday" if Date.today.wday == 1
+    #
+    # @return   [void]
+    def file_patterns=(file_patterns)
+      if file_patterns.nil? || file_patterns.zero?
+        return
+      end
+
+      @matched_files = [] if @matched_files.nil?
+      (git.modified_files + git.added_files).each do |line|
+        file_patterns.each do |pattern|
+          @matched_files += [line] unless line.match(pattern).nil?
+        end
+      end
+    end
+
+    # Setup the file extesions as you need
+    #
+    #
+    # @return   [void]
+    def file_extensions=(file_extensions)
+      if file_extensions.nil? || file_extensions.zero?
+        return
+      end
+
+      extensions = file_extensions.reduce do |total, extension|
+        return total + "|" + extension
+      end
+
+      extensions_regex = "(?-mix:^[^\\.].*\\.(" + extensions + ")$)"
+
+      @matched_files = [] if @matched_files.nil?
+      (git.modified_files + git.added_files).each do |line|
+        @matched_files += [line] unless line.match(extensions_regex).nil?
+      end
+    end
+
+    # Start to check
+    #
+    #
+    # @return   [void]
+    def check(check_patterns)
+      puts check_patterns
+      puts "---"
+      puts @matched_files
+
+      puts "---"
+      puts git.modified_files
+      puts "---"
+      puts git.added_files
+      puts "---"
+      # puts @matched_files
+
+      # @matched_files.each do |file|
+      #   puts git.diff_for_file(file)
+      # end
     end
   end
 end
